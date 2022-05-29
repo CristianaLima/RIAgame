12f87dd658ae2c3d2f8ba92134cc0baa86137fd5
Cristiana <limacoelhocristiana@gmail.com>
2022-05-29 15:32:41 +0200

    Show all

--- score.js
+++ score.js
@@ -1,76 +1,72 @@
 const TableScore =JSON.parse( localStorage.getItem('TableScore'));
 const BodyTable = document.getElementById("tableBody");
+let show = false;
 
 (function(){
-    var row = 0;
-    var classement = 1;
-    var premier=2, deuxieme=1, troisieme=0;
-    var tab1, tab2, tab3 ;
-    
-    TableScore.table.forEach(element =>{
-        if(element.TableScore>= premier){
-            troisieme=deuxieme;
-            deuxieme=premier;
-            premier= element.TableScore;
 
-            tab3 = tab2;
-            tab2 =tab1;
-            tab1 = element;
+    for(var i=1; i<TableScore.table.length; i++){
+        var place =i;
+        var echangeScore=TableScore.table[i].TableScore;
+        var echangeName = TableScore.table[i].Name;
+        var echangeTime = TableScore.table[i].Time;
+        while(place!= -1){
+            if(place== 0 || echangeScore < TableScore.table[place-1].TableScore){
+                TableScore.table[place].TableScore = echangeScore;
+                TableScore.table[place].Name = echangeName;
+                TableScore.table[place].Time = echangeTime;
+
+                place =-1;
+            }
+            else{
+                TableScore.table[place].TableScore = TableScore.table[place-1].TableScore;
+                TableScore.table[place].Name = TableScore.table[place-1].Name;
+                TableScore.table[place].Time = TableScore.table[place-1].Time;
+
+                place = place-1;
+            }
         }
-        else if(element.TableScore>= deuxieme){
-            troisieme=deuxieme;
-            deuxieme= element.TableScore;
+    }
 
-            tab3 = tab2;
-            tab2 = element;
-        }
-        else if(element.TableScore >= troisieme){
-            troisieme = element.TableScore;
-            tab3 = element;
-        }
-    });
-
-    var tab ={"table" :[tab1, tab2, tab3]};
-
-    tab.table.forEach(element => {
-        var newLine = BodyTable.insertRow(row);
-        row +=1;
+    for(var i=1; i<4; i++){
+        var newLine = BodyTable.insertRow(i-1);
         var newColumn0 = newLine.insertCell(0);
         var newColumn1 = newLine.insertCell(1);
         var newColumn2 = newLine.insertCell(2);
         var newColumn3 = newLine.insertCell(3);
         
-        var position = document.createTextNode(classement);
-        classement +=1;
+        var position = document.createTextNode(i);
 
-        var name = document.createTextNode(element.Name);
-        var jump = document.createTextNode(element.TableScore);
-        var time = document.createTextNode(element.Time);
-
-        newColumn0.appendChild(position);
-        newColumn1.appendChild(name);
-        newColumn2.appendChild(jump);
-        newColumn3.appendChild(time);  
-    })
-
-    /* TableScore.table.forEach(element => {
-        var newLine = BodyTable.insertRow(row);
-        row +=1;
-        var newColumn0 = newLine.insertCell(0);
-        var newColumn1 = newLine.insertCell(1);
-        var newColumn2 = newLine.insertCell(2);
-        var newColumn3 = newLine.insertCell(3);
-
-        var position = document.createTextNode(classement);
-        classement +=1;
-
-        var name = document.createTextNode(element.Name);
-        var jump = document.createTextNode(element.TableScore);
-        var time = document.createTextNode("00:05");
+        var name = document.createTextNode(TableScore.table[i-1].Name);
+        var jump = document.createTextNode(TableScore.table[i-1].TableScore);
+        var time = document.createTextNode(TableScore.table[i-1].Time);
 
         newColumn0.appendChild(position);
         newColumn1.appendChild(name);
         newColumn2.appendChild(jump);
-        newColumn3.appendChild(time);    
-    }); */
+        newColumn3.appendChild(time); 
+    }
 })();
+
+function showAll(){
+    if(!show){
+        for(var i=3; i<TableScore.table.length; i++){
+            var newLine = BodyTable.insertRow(i);
+            var newColumn0 = newLine.insertCell(0);
+            var newColumn1 = newLine.insertCell(1);
+            var newColumn2 = newLine.insertCell(2);
+            var newColumn3 = newLine.insertCell(3);
+            
+            var position = document.createTextNode(i+1);
+
+            var name = document.createTextNode(TableScore.table[i].Name);
+            var jump = document.createTextNode(TableScore.table[i].TableScore);
+            var time = document.createTextNode(TableScore.table[i].Time);
+
+            newColumn0.appendChild(position);
+            newColumn1.appendChild(name);
+            newColumn2.appendChild(jump);
+            newColumn3.appendChild(time); 
+        }
+        show=true;
+    }
+}
